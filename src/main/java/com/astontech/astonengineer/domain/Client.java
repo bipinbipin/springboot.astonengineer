d5632d7e56c5bebbb8f7efdc57733d5be60ec12a
bipin <bipinbipin@gmail.com>
2015-11-09 11:44:54 -0600

    Test of commit process

--- src/main/java/com/astontech/astonengineer/domain/Client.java
+++ src/main/java/com/astontech/astonengineer/domain/Client.java
@@ -1,7 +1,15 @@
 package com.astontech.astonengineer.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by Bipin on 11/9/2015.
  */
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "client_id")
+    private Integer Id;
 }
